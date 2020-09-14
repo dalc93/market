df63a8df1192cacd8386ca9d759d2b1412a1713d
Diego Leal <dalc93@gmail.com>
2020-09-14 16:00:15 -0500

    Proyecto market

---
+++ src/test/java/com/platzimarket/DlealMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.platzimarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DlealMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
